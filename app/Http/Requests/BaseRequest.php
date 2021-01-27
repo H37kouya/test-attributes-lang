e774f0504342b1fecb87ef4372ded68543ea1e49
H37kouya <h37kouya@gmail.com>
2021-01-27 22:31:08 +0900

    init

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BaseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    public function attribute()
+    {
+        return [
+            'reset'     => __('passwords.reset'),
+            'sent'      => __('passwords.sent'),
+            'throttled' => __('passwords.throttled'),
+            'token'     => __('passwords.token'),
+            'user'      => __('passwords.user'),
+            'auth'      => [
+                'failed'    => __('auth.failed'),
+                'password'  => __('auth.password'),
+                'throttle'  => __('auth.throttle'),
+            ],
+            'failedKey' => __('nothing.key'),
+        ];
+    }
+}
---
+++ tests/Feature/App/Http/Requests/BaseRequestTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\App\Http\Requests;
+
+use App\Http\Requests\BaseRequest;
+use Illuminate\Support\Arr;
+use Tests\Feature\Traits\HasLang;
+use Tests\TestCase;
+
+class BaseRequestTest extends TestCase
+{
+    use HasLang;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    private function createRequest(): BaseRequest
+    {
+        return new BaseRequest();
+    }
+
+    /**
+     * @param string $attribute
+     * @dataProvider requestAttributeProvider
+     */
+    public function testPassing_attributesの翻訳が行われているか(string $attribute)
+    {
+        $this->assertHasLangMultipleLocales($attribute, $this->locales, null);
+    }
+
+    public function requestAttributeProvider(): array
+    {
+        $this->createApplication();
+        $this->setUpHasLang();
+
+        $request = $this->createRequest();
+        $flatten = Arr::flatten($request->attribute());
+        return array_map(
+            fn ($v) => [ $v ],
+            $flatten
+        );
+    }
+}
